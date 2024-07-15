2bd35e2f11540d04a8eccab7d6cb951f941d53c4
Sami <samirazzaq@outlook.com>
2024-07-15 23:24:36 +0500

    Task2 Complete

--- Task 2/task2.py
+++ Task 2/task2.py
@@ -4,14 +4,16 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
 
 data = pd.read_csv("./emails.csv")
+
+print(data.head())
 
 # Get Features and Labels from dataset
 # Labels = either  given email is spam or not
 labels = data['Prediction']
 features = data.drop(['Email No.', 'Prediction'], axis=1)
-
 labels = np.array(labels)
 features = np.array(features)
 
@@ -22,3 +24,8 @@ X_train, X_test, y_train, y_test=train_test_split(features, labels, test_size=0.
 # Training  
 nb = MultinomialNB()
 nb.fit(X_train, y_train)
+
+# Testing on Test Data
+y_pred=nb.predict(X_test)
+accuracy = accuracy_score(y_pred, y_test)
+print('Accuracy is: ', accuracy)
